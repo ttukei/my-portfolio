4beba5e6a8fc251ad8b54a2c04e42191c9f9ce9e
timon <timon.l.tukei@gmail.com>
2024-11-29 16:23:07 -0800

    Fixed under construction page

--- app/components/AllSeeingEyeModel.tsx
+++ app/components/AllSeeingEyeModel.tsx
@@ -3,5 +3,14 @@ import { Canvas } from '@react-three/fiber';
 import clsx from 'clsx';
 
 const AllSeeingEyeModel = ({children, className}) => {
-
+    
+    return (
+        <Canvas className={clsx("w-screen h-screen relative", className)}>
+            <Suspense fallback={null}>
+                {children}
+            </Suspense>
+        </Canvas>
+    )
 }
+
+export default AllSeeingEyeModel;
